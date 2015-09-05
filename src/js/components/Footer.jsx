62c66c75b37fd2b7bc03203c0a5441103663bdf9
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-09-05 14:46:02 -0600

    add footer component

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="android-footer mdl-mega-footer">
+        <div className="mdl-mega-footer--bottom-section">
+          <p className="mdl-typography--font-light">Sikuli Lab: © 2015</p>
+        </div>
+      </footer>
+    );
+  }
+}
