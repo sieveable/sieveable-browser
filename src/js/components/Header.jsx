113f8939e17d09ba65a68541672ddcf973862134
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-09-05 14:46:55 -0600

    redesign header component

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import {Link} from "react-router";
 
 export default class Header extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-fixed-top header">
-        <div className="container-fluid">
-          <div class="navbar-header">
-            <Link className="navbar-brand" to="/">Sieveable</Link>
-          </div>
+      <div className="android-header mdl-layout__header mdl-layout__header--waterfall">
+        <div className="mdl-layout__header-row">
+          <span className="mdl-layout-title">
+            Sieveable-Browser
+          </span>
         </div>
-      </nav>
+      </div>
     );
   }
 }
