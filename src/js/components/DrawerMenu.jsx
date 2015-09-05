e4da882a997d86ce6d8d5f8c75537be823f47a6b
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-09-05 15:33:55 -0600

    add external links

--- src/js/components/DrawerMenu.jsx
+++ src/js/components/DrawerMenu.jsx
@@ -16,14 +16,30 @@ export default class DrawerMenu extends React.Component {
           <div className="android-drawer-separator"></div>
 
           <span className="mdl-navigation__link" href="">Documentation</span>
-          <a className="mdl-navigation__link" href="">Examples</a>
-          <a className="mdl-navigation__link" href="">Reference</a>
-          <a className="mdl-navigation__link" href="">Guides</a>
+          <a className="mdl-navigation__link"
+            href="https://github.com/sikuli/sieveable/wiki/100--Examples"
+            target="_blank">
+            Examples
+          </a>
+          <a className="mdl-navigation__link"
+            href="https://github.com/sikuli/sieveable/wiki"
+            target="_blank">
+            Reference
+          </a>
+
           <div className="android-drawer-separator"></div>
 
           <span className="mdl-navigation__link" href="">Sieveable</span>
-          <a className="mdl-navigation__link" href="">About Sieveable</a>
-          <a className="mdl-navigation__link" href="">Create an issue</a>
+          <a className="mdl-navigation__link"
+            href="http://sieveable.io/about"
+            target="_blank">
+            About Sieveable
+          </a>
+          <a className="mdl-navigation__link"
+            href="https://github.com/sikuli/sieveable/issues"
+            target="_blank">
+            Create an issue
+          </a>
         </nav>
       </div>
     );
