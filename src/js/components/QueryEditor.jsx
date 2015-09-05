8af995e9163ec9a3b7cb4c42505adaf6b367e4ef
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-09-05 15:35:02 -0600

    add query editor group buttons

--- src/js/components/QueryEditor.jsx
+++ src/js/components/QueryEditor.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import QueryEditorToolbar from "./QueryEditorToolbar";
 import AceEditor from "react-ace";
 import "brace";
 import "brace/theme/tomorrow";
+import "brace/mode/xml";
 
 export default class QueryEditor extends React.Component {
 
   constructor(props) {
     super(props);
+    this.handleClearQuery = this.handleClearQuery.bind(this);
+    this.handleSubmitQuery = this.handleSubmitQuery.bind(this);
+    this.handleSaveQuery = this.handleSaveQuery.bind(this);
     this.state = {
       hasQuery: false,
       queryText: "MATCH app\nWHERE\n\t<Button/>\nRETURN app"
@@ -14,32 +19,42 @@ export default class QueryEditor extends React.Component {
   }
   render() {
     return (
-      <div className="panel panel-default">
-        <div className="panel-body">
-          <AceEditor editorProps={
-              {$blockScrolling: true}}
-              fontSize={20}
-              height="120px"
-              name="query-editor"
-              onChange={e => this.onChange(e)}
-              theme="tomorrow"
-              useWorker={false}
-              value={this.state.queryText}
-              width="100%"/>
-          <button
-            className={"btn btn-primary pull-right "
-              + (this.state.hasQuery? "active" : "disabled")}
-            onClick={e => this.handleSubmitQuery(e)} type="button">
-            Run
-          </button>
+      <div className="mdl-grid">
+        <div className="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+          <AceEditor
+            editorProps={{$blockScrolling: true}}
+            fontSize={20}
+            height="120px"
+            name="query-editor"
+            onChange={e => this.onChange(e)}
+            theme="tomorrow"
+            useWorker={false}
+            mode="xml"
+            value={this.state.queryText}
+            width="100%"/>
+          <QueryEditorToolbar
+            hasQuery={this.state.hasQuery}
+            queryText={this.state.queryText}
+            submitQuery={this.handleSubmitQuery}
+            clearQuery={this.handleClearQuery}
+            saveQuery={this.handleSaveQuery} />
         </div>
       </div>
-
     );
   }
+
   handleSubmitQuery(e) {
     this.props.submit(this.state.queryText);
   }
+
+  handleSaveQuery(e) {
+    console.log("I will save your query later...");
+  }
+
+  handleClearQuery() {
+    this.setState({queryText: ""});
+  }
+
   onChange(editorText) {
     this.setState({
       queryText: editorText
---
+++ src/js/components/QueryEditorToolbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactZeroClipboard from "react-zeroclipboard";
+
+export default class QueryEditorToolbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="editor-btn-group">
+        <button className={" mdl-button mdl-js-button mdl-button--fab " +
+          (this.props.hasQuery ? "active" : "disabled")}
+          id="btn-run"
+          onClick={e => this.props.submitQuery(e)}
+          type="button">
+          <i className="material-icons">play_arrow</i>
+        </button>
+        <button className={"mdl-button mdl-js-button mdl-button--fab " +
+          (this.props.hasQuery ? "active" : "disabled")}
+          id="btn-clear"
+          onClick={() => this.props.clearQuery()}
+          type="button">
+          <i className="material-icons">clear</i>
+        </button>
+        <ReactZeroClipboard text={this.props.queryText}>
+          <button className={"mdl-button mdl-js-button mdl-button--fab " +
+            (this.props.hasQuery ? "active" : "disabled")}
+            id="btn-copy"
+            type="button">
+            <i className="material-icons">content_copy</i>
+          </button>
+        </ReactZeroClipboard>
+        <button className={"mdl-button mdl-js-button mdl-button--fab " +
+          (this.props.hasQuery ? "active" : "disabled")}
+          id="btn-save"
+          onClick={e => this.props.saveQuery(e)}
+          type="button">
+          <i className="material-icons">star</i>
+        </button>
+      </div>
+    );
+  }
+}
