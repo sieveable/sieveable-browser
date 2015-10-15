d007ec2d540dedfa103b12ba84121632815dd2ff
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-10-15 13:52:45 -0600

    remove unused save links

--- src/js/components/DrawerMenu.jsx
+++ src/js/components/DrawerMenu.jsx
@@ -5,16 +5,11 @@ export default class DrawerMenu extends React.Component {
   render() {
     return (
       <div className="android-drawer mdl-layout__drawer">
-        <span className="mdl-layout-title">
+        <span className="mdl-layout-title text-center">
           Sieveable-Browser
+          BETA
         </span>
         <nav className="mdl-navigation">
-          <span className="mdl-navigation__link" href="">My Sieveable</span>
-          <a className="mdl-navigation__link" href="">Saved queries</a>
-          <a className="mdl-navigation__link" href="">Server configuration</a>
-          <a className="mdl-navigation__link" href="">Dataset information</a>
-          <div className="android-drawer-separator"></div>
-
           <span className="mdl-navigation__link" href="">Documentation</span>
           <a className="mdl-navigation__link"
             href="https://github.com/sikuli/sieveable/wiki/100--Examples"
